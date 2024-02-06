2ae97282c313dba51960c7276c939ce7008b7d61
Andreas Wolff <wandrease@gmail.com>
2024-02-06 11:07:08 +0200

    assignment 9

--- assignment_09/inaltimi.cpp
+++ assignment_09/inaltimi.cpp
@@ -2,6 +2,29 @@
 
 using namespace std;
 
+void swap(int *x, int *y) {
+  int tmp = *x;
+  *x = *y;
+  *y = tmp;
+}
+
+void bubbleSort(int list[], int len) {
+  int swapped;
+  for (int i = 0; i < len - 1; i++) {
+    swapped = 0;
+    for (int j = 0; j < len - i - 1; j++) {
+      if (list[j] > list[j + 1]) {
+        swap(&list[j], &list[j + 1]);
+        swapped = 1;
+      }
+    }
+    if (swapped == 0) {
+      break;
+    }
+    len--;
+  }
+}
+
 void categorize_athletes(int *heights, int num_of_athletes, int given_height) {
   int taller[num_of_athletes];
   int shorter[num_of_athletes];
@@ -9,10 +32,10 @@ void categorize_athletes(int *heights, int num_of_athletes, int given_height) {
 
   for (int i = 0; i < num_of_athletes; ++i) {
     if (heights[i] > given_height || heights[i] == given_height) {
-      taller[i] = i + 1;
+      taller[i] = heights[i];
       shorter[i] = 0;
     } else {
-      shorter[i] = i + 1;
+      shorter[i] = heights[i];
       taller[i] = 0;
     }
   }
@@ -21,6 +44,9 @@ void categorize_athletes(int *heights, int num_of_athletes, int given_height) {
     combined_heights[0][i] = shorter[i];
     combined_heights[1][i] = taller[i];
   }
+
+  bubbleSort(combined_heights[0], num_of_athletes);
+  bubbleSort(combined_heights[1], num_of_athletes);
 
   for (int i = 0; i < 2; ++i) {
     for (int j = 0; j < num_of_athletes; ++j) {
@@ -49,5 +75,5 @@ int main() {
 
   categorize_athletes(heights, num_of_athletes, given_height);
 
-  return 1;
+  return 0;
 }
